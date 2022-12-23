4ebb2b8d4a6ec3779d04f24bba0e38f3ded546a7
Taylor Massey <tamassey92@gmail.com>
2022-12-22 18:01:37 -0700

    commit tree 6bbbf3054bb49b32f6624ec6b885545b51a935c8

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const farmAnimals = 'cow horse sheep pig chicken';
 
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+const [bessie, ,dolly, babe, little] = farmAnimals.split(' ');
+const [blackAndWhite, ,black, pink, ,] = farmAnimals.split(' ');
+
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+const [r, o, y, g, b, , v] = colors;
+const [ , , , , , indg, ,] = colors;
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -9,6 +17,8 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+
+const { muppetName, color, song, job, partner } = muppet;
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -35,8 +45,7 @@ const nestedMuppet = {
 
 // Arrays
 
-// 4. Use destructuring to assign appropriate variables using the color names.
-
+// 4. Use destructuring to assign appropriate variables using the color names
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
